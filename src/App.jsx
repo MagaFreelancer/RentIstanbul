88c859395e64f1f3ab1f53084db8b78fd002ea8f
YakhyaevGadji <gadji180105@gmail.com>
2024-02-13 22:56:31 +0300

    add page Cars

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import { Header, Footer } from "./components";
 import "./App.scss";
+import Cars from "./pages/Home/Cars/Cars";
 function App() {
   return (
     <div className="app">
@@ -9,6 +10,7 @@ function App() {
       <main className="content">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/cars" element={<Cars />} />
         </Routes>
       </main>
       <Footer />
