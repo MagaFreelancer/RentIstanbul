1858a9222ba605e3dd39cdeeaf8e81ee266020ee
YakhyaevGadji <gadji180105@gmail.com>
2024-02-14 21:05:23 +0300

    render Cars

--- src/pages/Home/Cars/Cars.jsx
+++ src/pages/Home/Cars/Cars.jsx
@@ -1,6 +1,40 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./Cars.scss";
+import { fetchCars } from "../../../redux/slices/carSlice";
+import CarBlock from "../../../components/CarBlock";
+import CarSkeleton from "../../../components/CarSkeleton/CarSkeleton"
+
 const Cars = () => {
+    const { items, status } = useSelector((state) => state.car);
+    const dispatch = useDispatch();
+
+    React.useEffect(() => {
+        getCars();
+    }, []);
+
+    const getCars = async () => {
+        dispatch(fetchCars());
+    };
+
+    const cars = items.map((obj, index) => <CarBlock key={index} {...obj} />);
+    const skeletons = [...new Array(11)].map((_, index) => (
+      <CarSkeleton key={index} />
+    ));
+
     return (
-        <h1>Cars</h1>
+        <section className="cars">
+            <div className="container">
+                <div className="cars__inner">
+                    <h1 className="cars__name">Машины</h1>
+                    
+                    <button>Сортровка по Машин</button>
+                </div>
+                <ul className="cars__list">
+                    {status === 'success' ? cars : skeletons}
+                </ul>
+            </div>
+        </section>
     );
 }
  
