3fe6de48ea8a3f9a67766b0729a10f3163aa1756
Magomed Elmurzaev <ban0le4@gmail.com>
2024-02-21 19:20:45 +0300

    fetch

--- src/components/FilterPrice/index.jsx
+++ src/components/FilterPrice/index.jsx
@@ -1,41 +1,51 @@
-import { Slider } from "@mui/material";
 import React from "react";
+import Slider from "@mui/material/Slider";
 
 const FilterPrice = () => {
-    const minDistance = 10;
+  const minDistance = 10;
 
-    const [value1, setValue1] = React.useState([350, 650]);
+  const [value1, setValue1] = React.useState([350, 650]);
 
-    const handleChange1 = (event, newValue, activeThumb) => {
-        if (!Array.isArray(newValue)) {
-          return;
-        }
+  const handleChange1 = (event, newValue, activeThumb) => {
+    if (!Array.isArray(newValue)) {
+      return;
+    }
 
-        if (activeThumb === 0) {
-            setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
-        } else {
-            setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
-        }
-    };
+    if (activeThumb === 0) {
+      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+    } else {
+      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+    }
+  };
 
-    return (
-        <div className="cars__price">
-            <h2 className="cars__paragraph">Фильтр по цене</h2>
-            <div className="cars__inpnumber">
-              <input className="cars__price-input" type="number" value={value1[0]} placeholder="10 ₽"/>
-              <span>-</span>
-              <input className="cars__price-input" type="number" value={value1[1]} placeholder="1000 ₽"/>
-            </div>
-            <Slider
-              getAriaLabel={() => "Minimum distance"}
-              value={value1}
-              onChange={handleChange1}
-              min={10}
-              max={1000}
-              disableSwap
-            />
-        </div>
-    );
-}
- 
+  return (
+    <div className="cars__price">
+      <h2 className="cars__paragraph">Фильтр по цене</h2>
+      <div className="cars__inpnumber">
+        <input
+          className="cars__price-input"
+          type="number"
+          value={value1[0]}
+          placeholder="10 ₽"
+        />
+        <span>-</span>
+        <input
+          className="cars__price-input"
+          type="number"
+          value={value1[1]}
+          placeholder="1000 ₽"
+        />
+      </div>
+      <Slider
+        getAriaLabel={() => "Minimum distance"}
+        value={value1}
+        onChange={handleChange1}
+        min={10}
+        max={1000}
+        disableSwap
+      />
+    </div>
+  );
+};
+
 export default FilterPrice;
--- src/components/FilterYears/index.jsx
+++ src/components/FilterYears/index.jsx
@@ -1,16 +1,16 @@
 const FilterYears = () => {
-    return (
-        <div className="cars__years">
-            <h2 className="cars__years-title">Год выпуска</h2>
-            <ul className="cars__years-list">
-              <li className="cars__years-item">test</li>
-              <li className="cars__years-item">test</li>
-              <li className="cars__years-item">test</li>
-              <li className="cars__years-item">test</li>
-              <li className="cars__years-item">test</li>
-            </ul>
-        </div>
-    );
-}
- 
+  return (
+    <div className="cars__years">
+      <h2 className="cars__years-title">Год выпуска</h2>
+      <ul className="cars__years-list">
+        <li className="cars__years-item">test</li>
+        <li className="cars__years-item">test</li>
+        <li className="cars__years-item">test</li>
+        <li className="cars__years-item">test</li>
+        <li className="cars__years-item">test</li>
+      </ul>
+    </div>
+  );
+};
+
 export default FilterYears;
