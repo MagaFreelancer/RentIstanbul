272f7383dca2598a4d842cd198bb0f5f9449b155
YakhyaevGadji <gadji180105@gmail.com>
2024-02-22 11:26:36 +0300

    change filter radio

--- src/components/FilterBox/index.jsx
+++ src/components/FilterBox/index.jsx
@@ -4,16 +4,19 @@ const FilterBox = () => {
             <h2 className="cars__box">Коробка передач</h2>
             <ul>
               <li className="cars__gearbox">
-                <input id="any" type="radio" defaultValue="any" name="gearbox" defaultChecked/>
-                <label htmlFor="any">Любая</label>
+                <input className="cars__gearbox-radio" id="any" type="radio" defaultValue="any" name="gearbox" defaultChecked/>
+                <span className="cars__radio-custom"></span>
+                <label className="cars__radio-label" htmlFor="any">Любая</label>
               </li>
               <li className="cars__gearbox">
-                <input id="mechanics" type="radio"  defaultValue="mechanics" name="gearbox"/>
-                <label htmlFor="mechanics">Механика</label>
+                <input className="cars__gearbox-radio" id="mechanics" type="radio"  defaultValue="mechanics" name="gearbox"/>
+                <span className="cars__radio-custom"></span>
+                <label className="cars__radio-label" htmlFor="mechanics">Механика</label>
               </li>
               <li className="cars__gearbox">
-                <input id="auto" type="radio" defaultValue="auto" name="gearbox"/>
-                <label htmlFor="auto">Автомат</label>
+                <input className="cars__gearbox-radio" id="auto" type="radio" defaultValue="auto" name="gearbox"/>
+                <span className="cars__radio-custom"></span>
+                <label className="cars__radio-label" htmlFor="auto">Автомат</label>
               </li>
             </ul>
         </div>
--- src/components/FilterEngine/index.jsx
+++ src/components/FilterEngine/index.jsx
@@ -4,17 +4,17 @@ const FilterEngine = () => {
             <h2 className="cars__box">Двигатель</h2>
             <ul>
                 <li className="cars__gearbox">
-                  <input className="cars__gearbox-checkbox" id="petrol" type="checkbox" defaultValue="petrol" name="gearbox" defaultChecked/>
+                  <input className="cars__gearbox-checkbox" id="petrol" type="checkbox" defaultValue="petrol" defaultChecked/>
                   <span className="cars__gearbox-custom"></span>
                   <label className="cars__gearbox-label" htmlFor="petrol">Бензин</label>
                 </li>
                 <li className="cars__gearbox">
-                  <input className="cars__gearbox-checkbox" id="diesel" type="checkbox" defaultValue="diesel" name="gearbox"/>
+                  <input className="cars__gearbox-checkbox" id="diesel" type="checkbox" defaultValue="diesel"/>
                   <span className="cars__gearbox-custom"></span>
                   <label className="cars__gearbox-label" htmlFor="diesel">Дизель</label>
                 </li>
                 <li className="cars__gearbox">
-                  <input className="cars__gearbox-checkbox" id="electro" type="checkbox" defaultValue="electro" name="gearbox"/>
+                  <input className="cars__gearbox-checkbox" id="electro" type="checkbox" defaultValue="electro"/>
                   <span className="cars__gearbox-custom"></span>
                   <label className="cars__gearbox-label" htmlFor="electro">Электо/Гибрид</label>
                 </li>
