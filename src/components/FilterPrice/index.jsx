d7bc42979c3692aa8f7cbfbc3ffaa04850b5e6c0
Magomed Elmurzaev <ban0le4@gmail.com>
2024-02-23 21:08:10 +0300

    merged commits

--- src/components/FilterPrice/index.jsx
+++ src/components/FilterPrice/index.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import Slider from "@mui/material/Slider";
 
 const FilterPrice = () => {
-  const minDistance = 10;
-
   const [value1, setValue1] = React.useState([350, 650]);
 
-  const handleChange1 = (event, newValue, activeThumb) => {
-    if (!Array.isArray(newValue)) {
-      return;
-    }
-
-    if (activeThumb === 0) {
-      setValue1([Math.min(newValue[0], value1[1] - minDistance), value1[1]]);
+  const handleChange = (event, newValue) => {
+    setValue1([...newValue]);
+  };
+  const changeValueOne = (value) => {
+    if (+value >= value1[1]) {
+      return false;
     } else {
-      setValue1([value1[0], Math.max(newValue[1], value1[0] + minDistance)]);
+      setValue1([+value, value1[1]]);
     }
   };
-
+  const changeValueTwo = (value) => {
+    if (+value <= value1[1]) {
+      return false;
+    } else {
+      setValue1([value1[1], +value]);
+    }
+  };
   return (
     <div className="cars__price">
       <h2 className="cars__paragraph">Фильтр по цене</h2>
@@ -26,8 +29,9 @@ const FilterPrice = () => {
           className="cars__price-input"
           type="number"
           value={value1[0]}
-          onChange={(e) => {
-            setValue1([+e.target.value, value1[1]]);
+          onChange={({ target }) => {
+            // setValue1([+e.target.value, value1[1]]);
+            changeValueOne(target.value);
           }}
           placeholder="10 ₽"
         />
@@ -36,15 +40,16 @@ const FilterPrice = () => {
           className="cars__price-input"
           type="number"
           value={value1[1]}
-          onChange={(e) => {
-            setValue1([value1[0], +e.target.value]);
+          onChange={({ target }) => {
+            // setValue1([+e.target.value, value1[1]]);
+            changeValueTwo(target.value);
           }}
           placeholder="1000 ₽"
         />
       </div>
       <Slider
         getAriaLabel={() => "Minimum distance"}
-        onChange={handleChange1}
+        onChange={handleChange}
         value={value1}
         min={10}
         max={1000}
