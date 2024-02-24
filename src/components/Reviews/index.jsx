d65c83946433263bee8d4ab733232c24ff85bd75
gadji <gadji180105@gmail.com>
2024-02-24 18:29:52 +0300

    return

--- src/components/Reviews/index.jsx
+++ src/components/Reviews/index.jsx
@@ -1,17 +1,6 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./Reviews.scss";
 
 const Reviews = () => {
-    let settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-    };
-
     const reviews = [
         {
             avatar: '', 
@@ -35,22 +24,20 @@ const Reviews = () => {
         <section className="reviews">
             <div className="container">
                 <h2 className="reviews__title">Отзывы</h2>
-                <ul>
-                    <Slider {...settings}>
-                        {reviews.map((review, index) => {
-                            return (
-                                <li key={index} className="reviews__list-item">
-                                    <div className="reviews__list-profil">
-                                        <svg className="reviews__list-avatar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                        </svg>
-                                        <h3 className="reviews__list-name">{review.name}</h3>
-                                    </div>
-                                    <p className="reviews__list-comment">{review.title}</p>
-                                </li>
-                            )
-                        })}
-                    </Slider>
+                <ul className="reviews__list">
+                    {reviews.map((review, index) => {
+                        return (
+                            <li key={index} className="reviews__list-item">
+                                <div className="reviews__list-profil">
+                                    <svg className="reviews__list-avatar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+                                    <h3 className="reviews__list-name">{review.name}</h3>
+                                </div>
+                                <p className="reviews__list-comment">{review.title}</p>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         </section>
--- src/components/SliderBlocks/index.jsx
+++ src/components/SliderBlocks/index.jsx
@@ -2,50 +2,11 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchCars } from "../../redux/slices/carSlice";
 import { CarSkeleton, CarBlock } from "../../components";
-import Slider from "react-slick";
 import "./SliderBlocks.scss";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
 
 export default function SliderBlocks({ title }) {
     const { items, status } = useSelector((state) => state.car);
     const dispatch = useDispatch();
-
-    let settings = {
-        dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-    };
 
     React.useEffect(() => {
       getCars();
@@ -62,15 +23,10 @@ export default function SliderBlocks({ title }) {
     return (
         <section className="slider-block">
             <div className="container slider-block__container">
-                
-                    <h2 className="slider-block__title title">{title}</h2>
-                    <div>
-                        <Slider {...settings}>
-                            {status === "success" ? pizzas : skeletons}
-                        </Slider>
-                    </div>
-                
-                
+                <h2 className="slider-block__title title">{title}</h2>
+                <div className="slider-block__content">
+                    {status === "success" ? pizzas : skeletons}
+                </div>
             </div>
         </section>
     );
