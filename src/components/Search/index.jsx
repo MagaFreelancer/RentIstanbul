fc5d16c2966b078e6a5d1fce480a22a9d2e38864
YakhyaevGadji <gadji180105@gmail.com>
2024-02-26 12:26:45 +0300

    add filter redux search

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setSearchValue } from "../../redux/slices/filterSlice";
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const text = useSelector((e) => e.filter.searchValue);
+  
+  const onChangeValue = (value) => {
+    dispatch(setSearchValue(value));  
+  };
+
   return (
     <div className="cars__top-search">
-      <input type="text" placeholder="Renault Clio..." />
+      <input onChange={(event) => onChangeValue(event.target.value)} value={text} type="text" placeholder="Renault Clio..." />
     </div>
   );
 };
