8d2f2a659a7b8bc58d9e69650ac63da87576e4d7
gadji <gadji180105@gmail.com>
2024-03-04 19:05:56 +0300

    change CarSkeleton

--- src/components/CarSkeleton/index.jsx
+++ src/components/CarSkeleton/index.jsx
@@ -2,20 +2,28 @@ import ContentLoader from "react-content-loader"
 
 const CarSkeleton = (props) => (
     <ContentLoader 
-    className="scroll"
     speed={2}
-    width={220}
-    height={258}
-    viewBox="0 0 280 258"
+    width={261.71}
+    height={289.88}
+    viewBox="0 0 261 289"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="5" y="7" rx="10" ry="10" width="223" height="141" /> 
-    <rect x="83" y="82" rx="0" ry="0" width="0" height="1" /> 
-    <rect x="9" y="203" rx="10" ry="10" width="94" height="14" /> 
-    <rect x="8" y="235" rx="10" ry="10" width="71" height="13" /> 
-    <rect x="8" y="176" rx="10" ry="10" width="169" height="19" />
+    <rect x="536" y="147" rx="3" ry="3" width="67" height="11" /> 
+    <rect x="503" y="158" rx="3" ry="3" width="140" height="11" /> 
+    <rect x="577" y="158" rx="3" ry="3" width="53" height="11" /> 
+    <rect x="548" y="142" rx="3" ry="3" width="72" height="11" /> 
+    <rect x="506" y="146" rx="3" ry="3" width="100" height="11" /> 
+    <rect x="574" y="147" rx="3" ry="3" width="37" height="11" /> 
+    <rect x="487" y="144" rx="3" ry="3" width="140" height="11" /> 
+    <rect x="509" y="151" rx="3" ry="3" width="173" height="11" /> 
+    <circle cx="576" cy="155" r="10" /> 
+    <circle cx="576" cy="533" r="9" /> 
+    <rect x="0" y="0" rx="10" ry="10" width="261" height="173" /> 
+    <rect x="0" y="190" rx="4" ry="4" width="150" height="22" /> 
+    <rect x="0" y="223" rx="5" ry="5" width="100" height="22" /> 
+    <rect x="0" y="261" rx="5" ry="5" width="70" height="15" />
   </ContentLoader>
 )
 
