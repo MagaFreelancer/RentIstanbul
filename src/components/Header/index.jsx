19d5e5de1eca171a477ee0e1aa21df5b478aac80
YakhyaevGadji <gadji180105@gmail.com>
2024-03-06 23:24:59 +0300

    add media mobile

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,40 +2,52 @@ import { Link, NavLink } from "react-router-dom";
 import Languages from "../Languages";
 import Currencies from "../Currencies";
 import "./Header.scss";
+import React from "react";
 
 function Header() {
+  const [open, setOpen] = React.useState(false);
+
+  if(open) {
+    document.body.classList.add('body-scroll');
+  }else {
+    document.body.classList.remove('body-scroll');
+  }
+
   return (
     <header className="header">
-      <div className="container">
-        <nav className="header__nav">
-          <Link to="/" className="header__logo logo">
-            RI
-          </Link>
+      <div className="container inner">
+        <Link to="/" className="header__logo logo">
+          RI
+        </Link>
+        <nav className={`header__nav ${open ? `header__nav--active` : ''}`}>
           <ul className="header__menu menu">
             <li className="header__item menu__item">
-              <NavLink className="header__link menu__link" to="/">
+              <NavLink onClick={() => setOpen(false)} className="header__link menu__link" to="/">
                 Home
               </NavLink>
             </li>
             <li className="header__item menu__item">
-              <NavLink className="header__link menu__link" to="cars">
+              <NavLink onClick={() => setOpen(false)} className="header__link menu__link" to="cars">
                 Cars
               </NavLink>
             </li>
 
             <li className="header__item menu__item">
-              <NavLink className="header__link menu__link" to="about">
+              <NavLink onClick={() => setOpen(false)} className="header__link menu__link" to="about">
                 About
               </NavLink>
             </li>
           </ul>
-          <ul className="header__funcs">
-            <li className="header__item">
-              <Languages />
-            </li>
-              <Currencies/>
-          </ul>
         </nav>
+        <ul className="header__funcs">
+          <li className="header__item">
+            <Languages />
+          </li>
+            <Currencies/>
+        </ul>
+        <div onClick={() => setOpen(!open)} className={`header__burger ${open ? 'header__burger--active' : ''}`}>
+          <span></span>
+        </div>
       </div>
     </header>
   );
